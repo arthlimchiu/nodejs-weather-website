06cf05a8b544feb620a9a0a1227b9dda6b9d7abd
Arth Limchiu <limchiuarth@gmail.com>
2020-08-07 22:27:13 +0800

    Add humidity in message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=70e099f2a71edf320cc296f912b82c7c&query=${latitude},${longitude}`;
-
+  console.log(latitude, longitude);
   request({ url, json: true}, (error, response) => {
     if (error) {
       callback('Unable to connect to weather service', null);
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
       callback('Unable to find location', null);
     } else {
       const { body } = response;
-      callback(null, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`);
+      callback(null, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. Humidity is ${body.current.humidity}.`);
     }
   });
 }
